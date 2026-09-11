d4d3f1bbb04c47985ced28390bee6631fd4eae28
josemandujano92 <jm-mandujano@hotmail.com>
2026-09-11 20:46:52 +0200

    Better slingshot placement and new image for readme file.

--- pinball/GamePanel.java
+++ pinball/GamePanel.java
@@ -44,7 +44,7 @@ class GamePanel extends JPanel implements Runnable, KeyListener {
     private Arrow arrowLeft, arrowRight;
     
     private Slingshot slingLeft, slingLeftExtension1, slingLeftExtension2;
-    private double slFirstVertexX = 0.2 * WIDTH - 15;
+    private double slFirstVertexX = 0.2 * WIDTH - 20;
     private double slFirstVertexY = 0.65 * HEIGHT - 60;
     private double slSecondVertexX = 0.2 * WIDTH + 20;
     private double slSecondVertexY = 0.65 * HEIGHT + 15;
@@ -52,7 +52,7 @@ class GamePanel extends JPanel implements Runnable, KeyListener {
     private double slThirdVertexY = 0.65 * HEIGHT + 20;
     
     private Slingshot slingRight, slingRightExtension1, slingRightExtension2;
-    private double srFirstVertexX = 0.8 * WIDTH + 15;
+    private double srFirstVertexX = 0.8 * WIDTH + 20;
     private double srFirstVertexY = 0.65 * HEIGHT - 90;
     private double srSecondVertexX = 0.8 * WIDTH - 20;
     private double srSecondVertexY = 0.65 * HEIGHT - 20;
